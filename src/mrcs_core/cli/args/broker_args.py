d57c6ad0d7d35701fcc61c2a4ee577a451ef9fad
Bruno Beloff <bruno.beloff@southcoastscience.com>
2025-12-12 18:03:15 +0000

    Added mrcs_publisher CLI tool

--- src/mrcs_core/cli/args/broker_args.py
+++ src/mrcs_core/cli/args/broker_args.py
@@ -6,12 +6,12 @@ Created on 17 Nov 2025
 https://realpython.com/command-line-interfaces-python-argparse/
 """
 
-from mrcs_core.cli.args.mrcs_args import MRCSArgs
+from mrcs_core.cli.args.core_args import CoreArgs
 
 
 # --------------------------------------------------------------------------------------------------------------------
 
-class BrokerArgs(MRCSArgs):
+class BrokerArgs(CoreArgs):
     """unix command line handler"""
 
     def __init__(self, description):
