9e5b507e01ef0cbb40249ecd72b571c5d25946db
Bruno Beloff <bruno.beloff@southcoastscience.com>
2026-07-19 09:32:08 +0100

    Refactored Segment inventory class

--- core_tests/unit/inventory/segment/test_segment_link.py
+++ core_tests/unit/inventory/segment/test_segment_link.py
@@ -14,7 +14,7 @@ import unittest
 
 from mrcs_core.data.json import JSONify
 from mrcs_core.equipment.turnout.turnout_enums import TurnoutPosition
-from mrcs_core.inventory.segment.segment_link import SegmentLink, TrackSegmentLink, TurnoutSegmentLink
+from mrcs_core.inventory.segment.segment_link import SimpleSegmentLink, SwitchedSegmentLink
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -22,102 +22,88 @@ from mrcs_core.inventory.segment.segment_link import SegmentLink, TrackSegmentLi
 class TestSegmentLink(unittest.TestCase):
 
     @classmethod
-    def __sample_track_segment_link_1(cls):
-        return TrackSegmentLink('BN01', 'S02')
+    def __sample_simple_segment_link_1(cls):
+        return SimpleSegmentLink('BN01', 'S02')
 
 
     @classmethod
-    def __sample_track_segment_link_2(cls):
-        return TrackSegmentLink('BN02', 'S01')
+    def __sample_simple_segment_link_2(cls):
+        return SimpleSegmentLink('BN02', 'S01')
 
 
     @classmethod
-    def __sample_turnout_segment_link(cls):
-        return TurnoutSegmentLink(cls.__sample_track_segment_link_1(), cls.__sample_track_segment_link_2())
+    def __sample_switched_segment_link(cls):
+        return SwitchedSegmentLink(cls.__sample_simple_segment_link_1(), cls.__sample_simple_segment_link_2())
 
 
     @classmethod
-    def __sample_turnout_segment_link_half_null(cls):
-        return TurnoutSegmentLink(cls.__sample_track_segment_link_1(), None)
+    def __sample_switched_segment_link_half_null(cls):
+        return SwitchedSegmentLink(cls.__sample_simple_segment_link_1(), None)
 
 
     def test_track_segment_link_str(self):
-        obj1 = self.__sample_track_segment_link_1()
-        self.assertEqual('TrackSegmentLink:{block_label:BN01, segment_label:S02}', str(obj1))
+        obj1 = self.__sample_simple_segment_link_1()
+        self.assertEqual('SimpleSegmentLink:{block_label:BN01, segment_label:S02}', str(obj1))
 
 
     def test_track_segment_link_jstr(self):
-        obj1 = self.__sample_track_segment_link_1()
+        obj1 = self.__sample_simple_segment_link_1()
         jstr = JSONify.dumps(obj1)
-        self.assertEqual('{"type": "TrackSegmentLink", "link": ["BN01", "S02"]}', jstr)
+        self.assertEqual('["BN01", "S02"]', jstr)
 
 
     def test_track_segment_jstr_eq(self):
-        obj1 = self.__sample_track_segment_link_1()
+        obj1 = self.__sample_simple_segment_link_1()
         jstr = JSONify.dumps(obj1)
-        obj2 = TrackSegmentLink.construct_from_jdict(json.loads(jstr))
+        obj2 = SimpleSegmentLink.construct_from_jdict(json.loads(jstr))
         self.assertEqual(obj2, obj1)
 
 
     def test_track_segment_link(self):
-        obj1 = self.__sample_track_segment_link_1()
+        obj1 = self.__sample_simple_segment_link_1()
         link = obj1.link_for_config(TurnoutPosition.P0)
-        self.assertEqual('TrackSegmentLink:{block_label:BN01, segment_label:S02}', str(link))
+        self.assertEqual('SimpleSegmentLink:{block_label:BN01, segment_label:S02}', str(link))
 
 
     def test_turnout_segment_link_str(self):
-        obj1 = self.__sample_turnout_segment_link()
-        self.assertEqual('TurnoutSegmentLink:{link_p0:TrackSegmentLink:{block_label:BN01, segment_label:S02}, '
-                         'link_p1:TrackSegmentLink:{block_label:BN02, segment_label:S01}}', str(obj1))
+        obj1 = self.__sample_switched_segment_link()
+        self.assertEqual('SwitchedSegmentLink:{link_p0:SimpleSegmentLink:{block_label:BN01, segment_label:S02}, '
+                         'link_p1:SimpleSegmentLink:{block_label:BN02, segment_label:S01}}', str(obj1))
 
 
     def test_turnout_segment_link_jstr(self):
-        obj1 = self.__sample_turnout_segment_link()
+        obj1 = self.__sample_switched_segment_link()
         jstr = JSONify.dumps(obj1)
-        self.assertEqual('{"type": "TurnoutSegmentLink", "p0": ["BN01", "S02"], "p1": ["BN02", "S01"]}', jstr)
+        self.assertEqual('{"p0": ["BN01", "S02"], "p1": ["BN02", "S01"]}', jstr)
 
 
     def test_turnout_segment_link_jstr_eq(self):
-        obj1 = self.__sample_turnout_segment_link()
+        obj1 = self.__sample_switched_segment_link()
         jstr = JSONify.dumps(obj1)
-        obj2 = TurnoutSegmentLink.construct_from_jdict(json.loads(jstr))
+        obj2 = SwitchedSegmentLink.construct_from_jdict(json.loads(jstr))
         self.assertEqual(obj2, obj1)
 
 
     def test_turnout_segment_link_half_null_jstr(self):
-        obj1 = self.__sample_turnout_segment_link_half_null()
+        obj1 = self.__sample_switched_segment_link_half_null()
         jstr = JSONify.dumps(obj1)
-        self.assertEqual('{"type": "TurnoutSegmentLink", "p0": ["BN01", "S02"], "p1": null}', jstr)
+        self.assertEqual('{"p0": ["BN01", "S02"], "p1": null}', jstr)
 
 
     def test_turnout_segment_link_half_null_jstr_eq(self):
-        obj1 = self.__sample_turnout_segment_link_half_null()
+        obj1 = self.__sample_switched_segment_link_half_null()
         jstr = JSONify.dumps(obj1)
-        obj2 = TurnoutSegmentLink.construct_from_jdict(json.loads(jstr))
+        obj2 = SwitchedSegmentLink.construct_from_jdict(json.loads(jstr))
         self.assertEqual(obj2, obj1)
 
 
     def test_turnout_segment_link_p0(self):
-        obj1 = self.__sample_turnout_segment_link()
+        obj1 = self.__sample_switched_segment_link()
         link = obj1.link_for_config(TurnoutPosition.P0)
-        self.assertEqual('TrackSegmentLink:{block_label:BN01, segment_label:S02}', str(link))
+        self.assertEqual('SimpleSegmentLink:{block_label:BN01, segment_label:S02}', str(link))
 
 
     def test_turnout_segment_link_p1(self):
-        obj1 = self.__sample_turnout_segment_link()
+        obj1 = self.__sample_switched_segment_link()
         link = obj1.link_for_config(TurnoutPosition.P1)
-        self.assertEqual('TrackSegmentLink:{block_label:BN02, segment_label:S01}', str(link))
-
-
-    def test_segment_link_track(self):
-        obj1 = self.__sample_track_segment_link_1()
-        jstr = JSONify.dumps(obj1)
-        obj2 = SegmentLink.construct_from_jdict(json.loads(jstr))
-        self.assertEqual(obj2, obj1)
-
-
-    def test_segment_link_turnout(self):
-        obj1 = self.__sample_turnout_segment_link()
-        jstr = JSONify.dumps(obj1)
-        obj2 = SegmentLink.construct_from_jdict(json.loads(jstr))
-        self.assertEqual(obj2, obj1)
+        self.assertEqual('SimpleSegmentLink:{block_label:BN02, segment_label:S01}', str(link))
