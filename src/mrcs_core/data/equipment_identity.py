4a1c0e3a8c238814f80d2ac4ac13d909a8b33a2e
Bruno Beloff <bruno.beloff@southcoastscience.com>
2026-08-02 09:20:16 +0100

    Added ControlRouterMonitorNode

--- src/mrcs_core/data/equipment_identity.py
+++ src/mrcs_core/data/equipment_identity.py
@@ -48,7 +48,7 @@ class EquipmentType(StrEnum, metaclass=MetaEnum):
         return self.value < other.value
 
 
-# TODO: create int enums of equipment numbers (for CRN)
+# TODO: create int enums of equipment numbers (for CRN and router interface)
 # --------------------------------------------------------------------------------------------------------------------
 
 class EquipmentSpecification(JSONable, ABC):
